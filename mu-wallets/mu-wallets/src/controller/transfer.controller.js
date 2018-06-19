95625260056861964ff058e436227738cb888909
jate-khr <jate.khr@mahidol.edu>
2018-06-19 14:27:04 +0700

    [Created] file transfer.controller.js

---
+++ mu-wallets/mu-wallets/src/controller/transfer.controller.js
@@ -0,0 +1,12 @@
+﻿var transferCrtl = function ($scope, $stateParams, $log, dataFactory) {
+    var ctrl = this;
+    ctrl.Transfer = function (accountNo, amount) {
+        dataFactory.Account.Transfer(ctrl.user.accountNo).then(function (data) {
+            ctrl.account.accountNo = data.accountNo;
+            ctrl.account.transfer = data.transfer;
+            ctrl.account.updateDate = data.updateDate;
+        });
+    }
+
+}
+transferCrtl.$inject = ['$scope', '$stateParams', '$log', 'dataFactory'];
