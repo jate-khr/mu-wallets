cb0156d35752e09aee95cc264dd24af18ee52a20
tum-surasak <surasak.rit@mahidol.edu>
2018-06-19 15:00:24 +0700

    [created] create test project

---
+++ mu-wallets/mu-wallet-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("mu-wallet-test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mu-wallet-test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("447405f5-2a9d-43b6-bdf4-b5f48f96569f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ mu-wallets/mu-wallet-test/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mu_wallet_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void transfer_total_amount_under_5000()
+        {
+            //arrange
+            int amount_transfer = 2000;
+            int amount_balance = 1000;
+            Transfer transfer_obj = new Transfer();
+            bool expect_value = true;
+            //action
+            bool actual_value = transfer_obj.canTransfer(amount_transfer, amount_balance);
+
+            //assert
+
+            Assert.AreEqual(expect_value, actual_value);
+        }
+        [TestMethod]
+        public void transfer_total_amount_equal_5000()
+        {
+            //arrange
+            int amount_transfer = 3000;
+            int amount_balance = 2000;
+            Transfer transfer_obj = new Transfer();
+            bool expect_value = true;
+            //action
+            bool actual_value = transfer_obj.canTransfer(amount_transfer, amount_balance);
+
+            //assert
+
+            Assert.AreEqual(expect_value, actual_value);
+        }
+    }
+}
