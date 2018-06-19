3e003a47153728bc86b94c4a6a6944ddd07bce45
tum-surasak <surasak.rit@mahidol.edu>
2018-06-19 15:34:16 +0700

    [created] create file displayTransfer.controller.js

---
+++ mu-wallets/mu-wallets/src/controller/displayTransfer.controller.js
@@ -0,0 +1,14 @@
+﻿var transferResultCrtl = function ($stateParams,dataFactory,$q) {
+    var accountNo = $stateParams.accountNo,
+        accountReceive = $stateParams.accountReceive,
+        amount = $stateParams.amount,
+        ctrl = this;
+    $q.all([
+        dataFactory.Account.GetBal(accountNo),
+        dataFactory.Account.GetBal(accountReceive)
+    ]).then(function (response) {
+        ctrl.sourceAccount = response[0].data;
+        ctrl.targetAccount = response[1].data;
+        });
+
+}
