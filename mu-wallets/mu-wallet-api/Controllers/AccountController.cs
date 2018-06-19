18ea97da584996eabc6729dd294db94b93a539cc
tum-surasak <surasak.rit@mahidol.edu>
2018-06-19 14:36:07 +0700

    [edited] add data and create AccountController Action GetBalance

---
+++ mu-wallets/mu-wallet-api/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using mu_wallet_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mu_wallet_api.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult GetBalance(string accountNo)
+        {
+            walletDbContext database = new walletDbContext();
+
+            return Json(database.Accounts.Find(accountNo), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
